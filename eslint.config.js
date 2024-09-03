a4cebbc1c9ef2be66881b0af368613e6fd2e77bf
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-03 21:05:42 +0200

    added packeges eslint and set configs

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
