c9f43ccff72337f978d25fa63c62c6223857c7d3
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-04 20:47:32 +0200

    add darkmode from materia ui

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,17 +1,23 @@
 // import { IconButton, Stack } from "@mui/material"
 import { Card, Typography } from "@mui/material"
 import { useQuestionsStore } from "./Store/questions"
+import SyntaxHighLighter from "react-syntax-highlighter"
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
 import { type Question as QuestionType } from "./types"
 
 
 const Question = ( { info }: {info: QuestionType}) => {
   return (
     <>
-    <Card variant='outlined'>
-      
+    <Card variant='outlined' sx={{ textAlign: 'left', padding: '5px', borderRadius:'10px', boxShadow: '0px 0px 20px rgba(255, 0, 0, 0.5)'}}>
+
       <Typography variant="h5">
         {info.question}
       </Typography>
+      <SyntaxHighLighter language="javascript" style={gradientDark}>
+        {info.code}
+      </SyntaxHighLighter>
     </Card>
     </>
   )
