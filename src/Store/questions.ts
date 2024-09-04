204672065ef11fd15c7100d51fcd68a37430b6fa
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-04 20:11:46 +0200

    add sinc await - whit local host data.json

--- src/Store/questions.ts
+++ src/Store/questions.ts
@@ -17,22 +17,11 @@ export const useQuestionsStore = create<State>((set, get) => {
     currentQuestion: 0,
 
     fetchQuestions: async (limit: number) => {
-     set({
-      questions: [
-        {
-          "id": 1,
-          "question": "¿Cuál es la salida de este código?",
-          "code": "console.log(typeof NaN)",
-          "answers": [
-            "undefined",
-            "NaN",
-            "string",
-            "number"
-          ],
-          "correctAnswer": 3
-        }
-      ]
-     })
+     const res = await fetch('http://localhost:5173/data.json')
+     const json = await res.json()
+
+     const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
+     set({ questions })
     }
   }
 })
