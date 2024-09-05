cd3fc9aa281bef45225145cc3321a15a49c9b444
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-05 19:01:45 +0200

    rm blanc space

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,7 +3,6 @@ import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from "
 import { useQuestionsStore } from "./Store/questions"
 import SyntaxHighLighter from "react-syntax-highlighter"
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-
 import { type Question as QuestionType } from "./types"
 
 
