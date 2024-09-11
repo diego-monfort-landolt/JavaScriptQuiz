c10223991c85463b548c60683bcc181454d05f91
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-11 18:13:50 +0200

    add script json and vite page

--- src/Store/questions.ts
+++ src/Store/questions.ts
@@ -19,7 +19,7 @@ export const useQuestionsStore = create<State>((set, get) => {
     currentQuestion: 0,
 
     fetchQuestions: async (limit: number) => {
-     const res = await fetch('http://localhost:5173/data.json')
+     const res = await fetch('https://diego-monfort-landolt.github.io/JavaScriptQuiz/data.json')
      const json = await res.json()
 
      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
