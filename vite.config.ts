02caaedab8f15b37a3a5fae22cafa35a1f35c2e3
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-09-11 18:07:00 +0200

    add script json and vite page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'htthttps://diego-monfort-landolt.github.io/JavaScriptQuiz/'
+  base:'https://diego-monfort-landolt.github.io/JavaScriptQuiz/'
 })
