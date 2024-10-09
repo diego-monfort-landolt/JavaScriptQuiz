20b20016c56302dca9d272fca66b08042dffb2f6
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-10-09 20:18:59 +0200

    added new style to btn new game

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer style={{ marginTop: '16px' }}>
       <strong>{`✅ ${correct} correctas - ❌ ${incorrect} incorrectas - ❓ ${unanswered} sin responder`}</strong>
       <div style={{ marginTop: '16px' }}>
-        <Button style={{ boxShadow: '0 0px 5px 1px red', marginTop: '10px', border: '1px solid red', borderRadius: '15px'}} onClick={() => reset()}>
+        <Button style={{ boxShadow: '0 0px 5px 1px red', marginTop: '10px', border: '1px solid red', borderRadius: '15px 0 15px 0'}} onClick={() => reset()}>
           New Game
         </Button>
       </div>
