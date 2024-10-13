47b2bfe5975ea5f6358bef30c25a82e9628bfd0e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-10-13 19:33:38 +0200

    a

--- src/Game.tsx
+++ src/Game.tsx
@@ -8,6 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import { Footer } from "./Footer"
 
 const Question = ( { info }: {info: QuestionType}) => {
+  
   const selectAnswer = useQuestionsStore(state => state.selectAnswer)
 
   const createHandleClick = (answerIndex: number) => () =>{
